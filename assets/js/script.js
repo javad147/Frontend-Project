95037cddb41a6bb5617148b376b49b005c478d4f
javad147 <javadnb@code.edu.az>
2024-03-26 12:42:53 +0400

    All photos were downloaded and 3 pages were created Index, Register, Blog filled footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 "use strict";
 
- 
+windowOn.on('load', function () {
+    $("#loading").fadeOut(500);
+});
+
+if ($('.tp-header-height').length > 0) {
+    var headerHeight = document.querySelector(".tp-header-height");      
+    var setHeaderHeight = headerHeight.offsetHeight;	
+    
+    $(".tp-header-height").each(function () {
+        $(this).css({
+            'height' : setHeaderHeight + 'px'
+        });
+    });
+  }
